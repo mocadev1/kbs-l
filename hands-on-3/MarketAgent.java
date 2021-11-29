ca32a1520809e5a77b3b5e2c286768d71a1a059b
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-11-29 14:45:58 -0600

    hands-on 3 TELL & ASK.

---
+++ hands-on-3/MarketAgent.java
@@ -0,0 +1,52 @@
+package test;
+
+import net.sf.clipsrules.jni.*;
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+
+public class MarketAgent extends Agent {
+
+    
+    
+    protected void setup() {
+        
+        try {
+            Environment clips = new Environment();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        System.out.println("Agent " + getLocalName() + " started.");
+
+        addBehaviour(new ReadClp());
+        addBehaviour(new ListAndExecute());
+    }
+
+    private class ReadClp extends OneShotBehaviour {
+
+        public void action() {
+            try {
+                clips.eval("(clear)");
+                clips.load("./resources/market/load-templates.clp");
+                clips.load("./resources/market/load-facts.clp");
+                clips.load("./resources/market/load-rules.clp");
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+        }
+    }
+
+    private class ListAndExecute extends OneShotBehaviour {
+
+        public void action() {
+            try {
+                clips.eval("(reset)");
+                clips.eval("(facts)");
+                clips.eval("(rules)");
+                clips.run();
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+        }
+    }
+}
